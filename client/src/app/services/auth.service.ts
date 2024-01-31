cc24daf4cf244c7a7e0961190a9e18500dd5c980
Rami Jebali <jebalirami7@gmail.com>
2024-01-31 12:49:11 +0100

    Update environment import in auth.service.ts and remove unused import in service.ts

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
