36ca183cd50b4958cc7a16e4dd3ab4633d0d3361
Rami Jebali <jebalirami7@gmail.com>
2024-01-31 13:20:35 +0100

    Add OPTIONS method to CORS configuration

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 app.use(cors(
     {
         origin: [process.env.CLIENT_URL],
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         credentials: true
     }
 ));
