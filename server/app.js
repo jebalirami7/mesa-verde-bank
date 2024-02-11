ff3696aefb9d68b177115a8c4d9c6575ec42d5d4
Rami Jebali <jebalirami7@gmail.com>
2024-02-11 21:07:58 +0100

    Add PATCH method to CORS configuration

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 app.use(cors(
     {
         origin: [process.env.CLIENT_URL],
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
         credentials: true
     }
 ));
