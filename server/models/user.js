a624ab8ae3a6df44f3b294b4988f5d91b17ad983
Rami Jebali <jebalirami7@gmail.com>
2024-02-20 18:15:23 +0100

    Add role and cin fields to user model and update user controller

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    username: {type: String, required : true},
-    password: {type: String, required : true},
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role: {type: String, required: true},
+    cin: {type: Number, required: true, unique: true},
 });
 
 module.exports = mongoose.model('User', userSchema) ; 
